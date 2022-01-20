f58ca04c6bbe7da8e5ca76cfc4b4c8fff06e7d28
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-20 20:15:04 -0300

    created logo components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+import { Logo } from "./components/Logo";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+      <Logo />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: grid;
+  place-items: center;
+
+  background-color: #275dad;
+`
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { GlobalStyle } from './global';
 
 ReactDOM.render(
-  <App />,
+  <>
+    <App />
+    <GlobalStyle />
+  </>,
   document.getElementById('root')
 );
